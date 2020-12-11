5dbd8f0da75f0f45aa054c88931ec7a7219c3089
Matt <gr8bitmail@gmail.com>
2020-12-10 19:27:37 -0600

    Fixed creative issues

---
+++ src/instruments/listeners/PlayerDrop.java
@@ -0,0 +1,27 @@
+package instruments.listeners;
+
+import instruments.Instrument;
+import instruments.Instruments;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerDropItemEvent;
+
+public class PlayerDrop implements Listener {
+
+    private Instruments instance = Instruments.getInstance();
+
+    @EventHandler
+    public void onItemDrop(PlayerDropItemEvent event) {
+        Player p = event.getPlayer();
+        if(!instance.getInstrumentManager().containsKey(p)) return;
+
+        Instrument instrument = instance.getInstrumentManager().get(p);
+
+        if(instrument.isHotBarMode()) {
+            event.setCancelled(true);
+            return;
+        }
+    }
+
+}
