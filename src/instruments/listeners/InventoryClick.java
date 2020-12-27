2c0f66e63807ce38c59d723f6d72ac1f1042801a
Achoowy <Achoowy@gmail.com>
2020-12-27 06:28:18 -0600

    Double Click Extra Note Bug Fixed

--- src/instruments/listeners/InventoryClick.java
+++ src/instruments/listeners/InventoryClick.java
@@ -14,106 +14,116 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.ClickType;
+import org.bukkit.event.inventory.InventoryAction;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.ItemStack;
 
 public class InventoryClick implements Listener {
 
-    private Instruments instance = Instruments.getInstance();
+	private Instruments instance = Instruments.getInstance();
 
-    @EventHandler
-    public void onInventoryClick(InventoryClickEvent e) {
-        Player p = (Player) e.getWhoClicked();
-        if(e.getClickedInventory() == null) return;
+	@EventHandler
+	public void onInventoryClick(InventoryClickEvent e) {
+		Player p = (Player) e.getWhoClicked();
+		if (e.getClickedInventory() == null)
+			return;
 
-        if(!instance.getInstrumentManager().containsKey(p)) return;
+		if (!instance.getInstrumentManager().containsKey(p))
+			return;
 
-        Instrument instrument = instance.getInstrumentManager().get(p);
+		Instrument instrument = instance.getInstrumentManager().get(p);
 
-        if(instrument.isHotBarMode()) {
-            return;
-        }
+		if (instrument.isHotBarMode()) {
+			return;
+		}
 
-        if (e.getView().getTitle().contains("Instruments:")
-                && !e.getClickedInventory().getType().equals(InventoryType.PLAYER)
-                && e.getView().getTopInventory().getType().equals(InventoryType.CHEST)) {
-            e.setCancelled(true);
+		if (e.getView().getTitle().contains("Instruments:")
+				&& !e.getClickedInventory().getType().equals(InventoryType.PLAYER)
+				&& e.getView().getTopInventory().getType().equals(InventoryType.CHEST)) {
+			e.setCancelled(true);
 
-            String itemName = ChatColor.stripColor(e.getCurrentItem().getItemMeta().getDisplayName());
+			// prevent double clicking causing an extra note to be played
+			if (e.getClick() == ClickType.DOUBLE_CLICK)
+				return;
+			
+			String itemName = ChatColor.stripColor(e.getCurrentItem().getItemMeta().getDisplayName());
 
-            if(e.getCurrentItem().getType().equals(Material.PAPER)) {
-                instrument.playScales();
-                return;
-            }
+			if (e.getCurrentItem().getType().equals(Material.PAPER)) {
+				instrument.playScales();
+				return;
+			}
 
-            if(itemName.contains("Chords")) {
-                instrument.getInstrumentInventory().toggleChords();
-                instrument.play();
-                return;
-            }
+			if (itemName.contains("Chords")) {
+				instrument.getInstrumentInventory().toggleChords();
+				instrument.play();
+				return;
+			}
 
-            org.bukkit.Instrument bukkitInstrument = org.bukkit.Instrument.valueOf(instrument.getInstrumentType().toString());
-            boolean useChords = instrument.getInstrumentInventory().getChordsEnabled();
+			org.bukkit.Instrument bukkitInstrument = org.bukkit.Instrument
+					.valueOf(instrument.getInstrumentType().toString());
+			boolean useChords = instrument.getInstrumentInventory().getChordsEnabled();
 
-            int octave = 1;
-            if(e.getRawSlot() > 26) octave = 0;
+			int octave = 1;
+			if (e.getRawSlot() > 26)
+				octave = 0;
 
-            // Player clicked a natural/sharp note
-            if(e.getCurrentItem().getType().equals(Material.WHITE_STAINED_GLASS_PANE) || e.getCurrentItem().getType().equals(Material.BLACK_STAINED_GLASS_PANE)) {
-                String note = ChatColor.stripColor(e.getCurrentItem().getItemMeta().getDisplayName()).charAt(0) + "";
+			// Player clicked a natural/sharp note
+			if (e.getCurrentItem().getType().equals(Material.WHITE_STAINED_GLASS_PANE)
+					|| e.getCurrentItem().getType().equals(Material.BLACK_STAINED_GLASS_PANE)) {
+				String note = ChatColor.stripColor(e.getCurrentItem().getItemMeta().getDisplayName()).charAt(0) + "";
 
-                if(useChords) {
-                    if(e.getClick().equals(ClickType.SHIFT_LEFT) || e.getClick().equals(ClickType.SHIFT_RIGHT)) {
-                        boolean minor = e.getClick().equals(ClickType.SHIFT_LEFT) ? true : false;
-                        String[] notes = minor ? Utils.getMinorTriad(note) : Utils.getMajorTriad(note);
-                        for(String pNote : notes) {
-                            instrument.playNote(pNote, octave);
-                        }
-                        return;
-                    }
-                }
+				if (useChords) {
+					if (e.getClick().equals(ClickType.SHIFT_LEFT) || e.getClick().equals(ClickType.SHIFT_RIGHT)) {
+						boolean minor = e.getClick().equals(ClickType.SHIFT_LEFT) ? true : false;
+						String[] notes = minor ? Utils.getMinorTriad(note) : Utils.getMajorTriad(note);
+						for (String pNote : notes) {
+							instrument.playNote(pNote, octave);
+						}
+						return;
+					}
+				}
 
-                instrument.playNote(note, octave);
-                return;
-            }
+				instrument.playNote(note, octave);
+				return;
+			}
 
-            return;
-        }
+			return;
+		}
 
-        if (e.getView().getTitle().contains("Scales")
-                && !e.getClickedInventory().getType().equals(InventoryType.PLAYER)
-                && e.getView().getTopInventory().getType().equals(InventoryType.CHEST)) {
-            e.setCancelled(true);
+		if (e.getView().getTitle().contains("Scales") && !e.getClickedInventory().getType().equals(InventoryType.PLAYER)
+				&& e.getView().getTopInventory().getType().equals(InventoryType.CHEST)) {
+			e.setCancelled(true);
 
-            String itemName = ChatColor.stripColor(e.getCurrentItem().getItemMeta().getDisplayName());
+			String itemName = ChatColor.stripColor(e.getCurrentItem().getItemMeta().getDisplayName());
 
-            if(itemName.contains(instrument.getInstrumentType().toString())) {
-                instrument.play();
-                return;
-            }
+			if (itemName.contains(instrument.getInstrumentType().toString())) {
+				instrument.play();
+				return;
+			}
 
-            if(e.getCurrentItem().getType().equals(Material.PAPER)) {
-                Scale selectedScale = Scale.getScaleByName(itemName);
+			if (e.getCurrentItem().getType().equals(Material.PAPER)) {
+				Scale selectedScale = Scale.getScaleByName(itemName);
 
-                if(selectedScale == null) return;
+				if (selectedScale == null)
+					return;
 
-                instrument.playHotbar(selectedScale);
-                return;
-            }
+				instrument.playHotbar(selectedScale);
+				return;
+			}
 
-            if(e.getCurrentItem().getType().equals(Material.ARROW)) {
-                int currPage = instrument.getScalesInventory().getPage();
-                if(itemName.contains("Next")) {
-                    instrument.getScalesInventory().setPage(currPage + 1);
-                } else {
-                    if(currPage != 0) {
-                        instrument.getScalesInventory().setPage(currPage - 1);
-                    }
-                }
-                instrument.playScales();
-                return;
-            }
-        }
-    }
+			if (e.getCurrentItem().getType().equals(Material.ARROW)) {
+				int currPage = instrument.getScalesInventory().getPage();
+				if (itemName.contains("Next")) {
+					instrument.getScalesInventory().setPage(currPage + 1);
+				} else {
+					if (currPage != 0) {
+						instrument.getScalesInventory().setPage(currPage - 1);
+					}
+				}
+				instrument.playScales();
+				return;
+			}
+		}
+	}
 }
